56abde3495404bf0e8f68b9eae1fc2cfbe39cd5f
Bestony <bestony@linux.com>
2025-10-07 11:32:37 +0800

    refactor: Remove unused import of debug_toolbar_urls from urls.py

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 
 """
 
-from debug_toolbar.toolbar import debug_toolbar_urls
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
